b7accc65c3410a3df4f1bff4f73388d08fa0212d
Rathnadeviprasath1 <rathna.deviprasath21@gmail.com>
2017-04-04 12:39:14 +0300

    Added the generated app Id and app key.

--- Assets/Scripts/Yle/YleProgramSearchService.cs
+++ Assets/Scripts/Yle/YleProgramSearchService.cs
@@ -11,8 +11,8 @@ namespace YleService
     public class YleProgramSearchService : MonoBehaviour
     {
         public string BaseUrl = "https://external.api.yle.fi";
-        public string AppId = "YOURAPPID";
-        public string AppKey = "YOURAPPKEY";
+		public string AppId = "74ffeb19";
+		public string AppKey = "3a4f62135d1c1b6e75698e3f88f0e76a";
 
         public UnityAction<List<YleProgram>, string> LoadProgramBatchFinished;
 
